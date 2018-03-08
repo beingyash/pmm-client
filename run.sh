64b690c4b35d53976d4508d414c0a6eb7556d33e
yash <being.yash>
2018-03-09 00:20:18 +0530

    Added remote pmm-client config in run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ pmm-admin config --server $MONITORING_SERVICE_NAME --client-name host_${HOSTNAME
 pmm-admin add mongodb --uri=$MONGODB_URI
 
 /usr/local/percona/pmm-client/node_exporter -web.listen-address=$CLIENT_ADDRESS:42000 -collectors.enabled=diskstats,filesystem,loadavg,meminfo,netdev,netstat,stat,time,uname,vmstat
+/bin/bash
