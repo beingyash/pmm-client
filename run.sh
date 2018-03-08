3d16477952f6227637f9012467efb3671f42902c
yash <being.yash>
2018-03-08 23:16:22 +0530

    Added remote pmm-client config in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-HOSTNAME=$(curl rancher-metadata/latest/self/host/name)
-pmm-admin config --server $MONITORING_SERVICE_NAME --client-name blabl --client-name host_${HOSTNAME}
+#HOSTNAME=$(curl rancher-metadata/latest/self/host/name)
+pmm-admin config --client-address=$CLIENT_ADDRESS --bind-address=$BIND_ADDRESS
+pmm-admin config --server $MONITORING_SERVICE_NAME --client-name host_${HOSTNAME}
 pmm-admin add linux:metrics
 /usr/local/percona/pmm-client/node_exporter -web.listen-address=$MY_IP:42000 -collectors.enabled=diskstats,filesystem,loadavg,meminfo,netdev,netstat,stat,time,uname,vmstat
