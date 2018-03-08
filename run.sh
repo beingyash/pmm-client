f2f2633c6293c98d600361c0d017f98d65bb4dc8
yash <being.yash>
2018-03-08 23:46:28 +0530

    Added remote pmm-client config in run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 #HOSTNAME=$(curl rancher-metadata/latest/self/host/name)
 pmm-admin config --client-address=$CLIENT_ADDRESS --bind-address=$BIND_ADDRESS
 pmm-admin config --server $MONITORING_SERVICE_NAME --client-name host_${HOSTNAME}
-pmm-admin add linux:metrics
-/usr/local/percona/pmm-client/node_exporter -web.listen-address=$MY_IP:42000 -collectors.enabled=diskstats,filesystem,loadavg,meminfo,netdev,netstat,stat,time,uname,vmstat
+pmm-admin add mongodb --uri=$MONGODB_URI
+
+/usr/local/percona/pmm-client/node_exporter -web.listen-address=$CLIENT_ADDRESS:42000 -collectors.enabled=diskstats,filesystem,loadavg,meminfo,netdev,netstat,stat,time,uname,vmstat
